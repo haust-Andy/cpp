23dc8e9e938c68175664ff831f082dc290cfcd90
wangdingbang <wangdingbang@xtourtec.com>
2023-08-15 11:07:53 +0800

    add base/Thread.h

---
+++ code/base/Thread.h
@@ -0,0 +1,37 @@
+
+
+#ifndef BASE_THREAD_H
+#define BASE_THREAD_H
+
+
+#include <functional>
+#include <thread>
+
+class Thread : noncopyable
+{
+public:
+    using ThreadFunc = std::function<void()>;
+
+    explicit Thread(ThreadFunc func, const string &name = string());
+    ~Thread();
+
+    void start();
+    void join();
+
+    const string &name() const { return _name; }
+    bool started() const { return _started; }
+
+private:
+    void setDefaultName();
+
+private:
+    bool _started;
+    bool _joined;
+    std::thread _thread;
+    ThreadFunc _func;
+    string _name;
+};
+
+
+
+#endif // !BASE_THREAD_H
